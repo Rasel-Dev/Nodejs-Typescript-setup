5a3d7de7b1be7bee43db00f7b956f62d2cfa07fd
Rasel Developer <dev.code.get@gmail.com>
2023-11-02 05:05:55 +0600

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ class Calculator {
     protected readonly total: number,
     protected readonly point: number
   ) {}
-
+  /**
+   * Default returning value will be poin percentage of total.
+   * if replacePoint have a value then it will be percent value of total
+   * @param replacePoint Percentage
+   * @returns number
+   */
   public toPercent(replacePoint?: number): number {
     return !replacePoint
       ? (100 * this.point) / this.total
